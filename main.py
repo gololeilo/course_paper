7e962676f57748bc922bbc59cc35d627698a8b3b
gololeilo <gololeilo@mail.ru>
2023-07-09 23:56:34 +1000

    ок

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-cvxcvcxv
+from utils import get_data, get_sorted_data, get_filtered_data, get_formatted
+
+
+def main():
+    print("курсовая")
+    data = get_data()
+    data = get_filtered_data(data)
+    data = get_sorted_data(data)
+    data = get_formatted(data)
+
+    print(data)
+
+
+
+if __name__ ==  '__main__':
+    main()
+
